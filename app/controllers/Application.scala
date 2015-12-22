15d213036c71c01c2bd2cc74e93eb0681a9ae285
ixguza <rtk0253@gmail.com>
2015-12-22 19:05:33 +0900

    新しい物を上に & タイムゾーン設定

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,33 +13,42 @@ object Application extends Controller {
 
   lazy val file = new File("log")
   lazy val timeZone = TimeZone.getTimeZone("Asia/Tokyo")
-  lazy val df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS")
+  lazy val df = {
+    val df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS")
+    df.setTimeZone(timeZone)
+    df
+  }
   lazy val valuekey = "value"
+
+  val usage = """0
+                |/write?value=hogeで書き込み,
+                |/resetでリセット
+                |=================================
+              """.stripMargin
+
 
   def index = Action {
     Ok {
-      if (file.exists) {
-        val lines = Source.fromFile(file).getLines.toList
+      val log: String = if (file.exists) {
+        val lines = Source.fromFile(file).getLines().toList.reverse
         lines.mkString(lines.size + "\n", "\n", "")
-      } else
-        """0
-          |/write?value=hogeで書き込み,
-          |/resetでリセット
-        """.stripMargin
+      } else ""
+
+      usage + log
     }
   }
 
   def reset = Action {
     file.delete
-    Ok("Reset!")
+    Redirect(routes.Application.index)
   }
 
   def write = Action { implicit request =>
     val value = request.getQueryString(valuekey).getOrElse("")
     val pw = new PrintWriter(new FileWriter(file, true))
-    val str = df.format(Calendar.getInstance(timeZone).getTime) + " " + value
+    val str = df.format(Calendar.getInstance().getTime) + " " + value
     pw.println(str)
-    pw.close
+    pw.close()
     Ok("Write : " + str)
   }
 }
